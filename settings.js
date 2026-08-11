c253978657693ee705c414eb3db595a1ae52be56
INATO-MD <ziyamariam24@gmail.com>
2026-08-12 03:51:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'INATO MD',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "INATO MD",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '916282144167', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
